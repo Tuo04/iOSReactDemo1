d6b58a75ea9600f16475aec3727df1f1a699c0f4
bys <bys@bysdeMBP-2.guest.corp.microsoft.com>
2017-07-05 14:03:15 +0800

    new

---
+++ iOSReact0705/ios/Pods/Target Support Files/Pods-iOSReact0705-tvOS/Pods-iOSReact0705-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOSReact0705_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_iOSReact0705_tvOS
+@end
---
+++ iOSReact0705/ios/Pods/Target Support Files/Pods-iOSReact0705-tvOSTests/Pods-iOSReact0705-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOSReact0705_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_iOSReact0705_tvOSTests
+@end
