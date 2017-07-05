4bf70f8431e1bbbd24332339c47f74918b682d16
Tuo04 <chentuo01@beyondsoft.com>
2017-07-04 20:13:10 -0700

    New Test

--- iOSReact0705/index.ios.js
+++ iOSReact0705/index.ios.js
@@ -5,8 +5,12 @@
  */
 
 import React, { Component } from 'react';
+import Push from 'mobile-center-push';
 import {
+  AppState, 
+  Alert,
   AppRegistry,
+  Button,
   StyleSheet,
   Text,
   View
@@ -19,17 +23,50 @@ export default class iOSReact0705 extends Component {
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Button
+          title="Crash Button"
+          onPress="onCrash"
+        />
       </View>
     );
   }
 }
+
+function onCrash(){
+  throw new Error("This is a Test JavaScript Crash");
+}
+
+class MyApp extends Component {
+}
+
+Push.setEventListener({
+  pushNotificationReceived: function (pushNotification) {
+    let message = pushNotification.message;
+    let title = pushNotification.title;
+
+    if (message === null || message === undefined) {
+      // Android messages received in the background don't include a message. On Android, that fact can be used to
+      // check if the message was received in the background or foreground. For iOS the message is always present.
+      title = "iOS background";
+      message = "<empty>";
+    }
+
+    // Custom name/value pairs set in the Mobile Center web portal are in customProperties
+    if (pushNotification.customProperties && Object.keys(pushNotification.customProperties).length > 0) {
+      message += '\nCustom properties:\n' + JSON.stringify(pushNotification.customProperties);
+    }
+
+    if (AppState.currentState === 'active') {
+      Alert.alert(title, message);
+    }
+    else {
+      // Sometimes the push callback is received shortly before the app is fully active in the foreground. This
+      // only seems to happen on Android, not iOS. In this case you'll want to save off the notification info
+      // and wait until the app is fully shown in the foreground before displaying any UI. You could
+      // use AppState.addEventListener to be notified when the app is fully in the foreground.
+    }
+  }
+});
 
 const styles = StyleSheet.create({
   container: {
